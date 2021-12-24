506969fd6b333cb9ec75e1b80d2cb86d1049760b
AntonyMorgante <antonymorgante1@gmail.com>
2021-12-24 14:24:20 +0100

    solved repeating numbers issue

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 let ar = [];
 let score = 0;
-let time = 5;
+let time = 30;
 let period;
 let gameIsOn = 0;
 
@@ -46,14 +46,32 @@ function writeNumbers(array){
     }
 }
 
-function askNumbers(array){
-    for (i=0; i<ar.length;i++){
-        let answer = parseInt(prompt("Inserisci uno dei numeri che erano presenti sullo schermo"));
-        for (j=0; j<ar.length;j++){
-            if (answer == ar[j]){
-                score ++
+function createAnswersArray(number){
+    let answers = [];
+    do {
+        n = parseInt(prompt("Inserisci uno dei numeri che erano visualizzati sullo schermo"))
+        let nIsUnique = true; 
+        for (let i=0; i<answers.length; i++){
+            if (answers[i] == n){ 
+                nIsUnique = false;
+                i = answers.length; 
+                alert("Hai già inserito questo numero!")
             }
         }
+        if (nIsUnique){
+            answers.push(n); 
+        }
+    } while (answers.length < number)
+    return answers;
+}
+
+function confrontAnswers(array1,array2){
+    for (let i=0;i<array1.length;i++){
+        for (let j=0;j<array2.length;j++){
+            if (array1[i]==array2[j]){
+                score += 1;
+            }
+        }   
     }
 }
 
@@ -63,6 +81,12 @@ function countdown(){
     countdownBox.innerHTML = time;
 }
 
+function initiateCountdown(){
+    let countdownBox = document.getElementById("countdown");
+    countdownBox.innerHTML = time;
+    period = setInterval(countdown, 1000);
+}
+
 function clearCountdown(){
     let countdownBox = document.getElementById("countdown");
     countdownBox.innerHTML = "";
@@ -70,7 +94,8 @@ function clearCountdown(){
 }
 
 function endGame(){
-    askNumbers(ar);
+    let answers = createAnswersArray(5);
+    confrontAnswers(ar,answers);
     let numberBox = document.getElementById("number-box");
     numberBox.innerHTML = "La tua partita è finita! Il tuo punteggio è: " + score + ".<br>Gioca di nuovo per provare a migliorare il tuo risultato!";
     gameIsOn = 0;
@@ -80,22 +105,20 @@ function endGame(){
 function newGameValues (){
     gameIsOn = 1;
     score = 0;
-    time = 5;
+    time = 30;
     ar = [];
 }
 
 function createGame(){
     if (!gameIsOn){
         newGameValues();
-        let countdownBox = document.getElementById("countdown");
-        countdownBox.innerHTML = time;
-        period = setInterval(countdown, 1000);
+        initiateCountdown();
         clearNumbers();
         createNumberArray(5); 
         writeNumbers(ar);
-        const clearCount = setTimeout(clearCountdown,4900);
-        const clearTime = setTimeout(clearNumbers,4900);
-        const questionTime = setTimeout(endGame,5000);
+        const clearCount = setTimeout(clearCountdown,29900);
+        const clearTime = setTimeout(clearNumbers,29900);
+        const questionTime = setTimeout(endGame,30000);
         playButton.innerHTML = "Partita in corso..."
     }
 }
